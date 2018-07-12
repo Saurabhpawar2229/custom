7c5d585a8c5036ae704dd183cf1c2232f10f1bfa
Saurabh Pawar <saurabhpawar82220@gmail.com>
2018-07-11 23:00:30 -0800

    custom.dir

---
+++ src/app/custom-style.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, } from '@angular/core';
+import { ElementRef,HostListener} from '@angular/core';
+
+
+@Directive({
+  selector: '[appCustomStyle]'
+})
+export class CustomStyleDirective {
+
+  constructor(private ele:ElementRef)
+   { 
+    
+     ele.nativeElement.style.background="yellow";
+     ele.nativeElement.style.fontWeight="bold";
+    
+  
+   }
+
+  
+
+}
